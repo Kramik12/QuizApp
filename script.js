8f4dad0dfae7ec52919e200d2ade0792d5eb07a4
Kramik <kramikkafle7@gmail.com>
2024-05-26 23:02:21 +0545

    first commit

--- script.js
+++ script.js
@@ -55,4 +55,11 @@ function showQuestion() {
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    })
 }
