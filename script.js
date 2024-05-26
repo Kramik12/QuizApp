e0eeb4bdc658b3bed086a4ad20e9ec421e8ea3a6
Kramik <kramikkafle7@gmail.com>
2024-05-26 17:58:32 +0545

    first commit

--- script.js
+++ script.js
@@ -40,3 +40,19 @@ const questions = [
 const questionElement = document.getElementById("quetsion");
 const answerButton = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+}
